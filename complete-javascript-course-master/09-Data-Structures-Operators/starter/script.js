2c3bb642857f1ad209adb579280ccd7f7ec4e222
Jorge Humberto <jorge8hgn@hotmail.com>
2025-06-18 16:07:58 -0700

    leccion 130

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -790,3 +790,134 @@ for (const [min, event] of gameEvents) {
 // LEC >> 128 - Working with Strings - Part 1
 // // || ******************************************
 // VV
+
+const airline = 'Tap Air Portugal';
+const plane = 'A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('portugal'));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😃');
+  else console.log('You got lucky 😎');
+};
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// console.log(new String('jonas'));
+// console.log(typeof new String('jonas'));
+
+// --
+// || ******************************************
+// LEC >> 129 - Working with Strings - Part 2
+// // || ******************************************
+// VV
+
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'JorGE'; //Jorge
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// Comparing email
+const email = 'hello@jonas.io';
+const loginEmail = '   Hello@Jonas.Io  \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizeEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizeEmail);
+// console.log(email === normalizeEmail);
+
+// Replacing
+const priceGB = '288,97E';
+const priceUs = priceGB.replace('E', '$').replace(',', '.');
+// console.log(priceUs);
+
+const announcement =
+  'All passengers come to barding door 23. Boarding door 23!';
+// console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+
+// console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane2 = 'Airbus A320neo';
+// console.log(plane2.includes('A320'));
+// console.log(plane2.includes('Boeing'));
+// console.log(plane2.startsWith('Air'));
+
+if (plane2.startsWith('Airbus') && plane2.endsWith('neo')) {
+  // console.log('Part of the NEW ARirbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+// checkBaggage('I have laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and a gun for protection');
+
+// --
+// || ******************************************
+// LEC >> 130 - Working with Strings - Part 3
+// // || ******************************************
+// VV
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jorge Humberto'.split(' '));
+
+const [firstName, lastName] = 'Jorge Humberto'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  // console.log(names);
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    const newN = n.toLowerCase();
+    namesUpper.push(newN.replace(newN[0], newN[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jorge humberto');
+capitalizeName('pedro picapiedRa');
