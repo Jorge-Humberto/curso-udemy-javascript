3958a666b6a4efa1b6ed47b501a485bea881c9fe
Jorge Humberto <jorge8hgn@hotmail.com>
2025-06-18 13:14:22 -0700

    inicializando el archivo

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -760,27 +760,33 @@ const gameEvents = new Map([
 //1. Create an array 'events' of the different game events that happened (no duplicates)
 const eventsSet = new Set(gameEvents.values());
 const eventsArray = [...eventsSet];
-console.log(eventsArray);
+// console.log(eventsArray);
 
 //2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 gameEvents.delete(64);
-console.log(gameEvents);
+// console.log(gameEvents);
 
 //3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-console.log(
-  `An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
 const time3 = [...gameEvents.keys()].pop();
-console.log(time3);
-console.log(
-  `An event happened, on average, every ${time3 / gameEvents.size} minutes`
-);
+// console.log(time3);
+// console.log(
+//   `An event happened, on average, every ${time3 / gameEvents.size} minutes`
+// );
 
 //4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL
 //let time2;
 for (const [min, event] of gameEvents) {
   const half = min <= 45 ? 'FIRST' : 'SECOND';
-  console.log(`[${half} HALF] ${min}: ${event}`);
+  // console.log(`[${half} HALF] ${min}: ${event}`);
   // key < 45 ? (time2 = `[FIRST HALF]`) : (time2 = `[SECOND HALF]`);
   // console.log(`${time2} ${min}: ${event}`);
 }
+
+// --
+// || ******************************************
+// LEC >> 128 - Working with Strings - Part 1
+// // || ******************************************
+// VV
