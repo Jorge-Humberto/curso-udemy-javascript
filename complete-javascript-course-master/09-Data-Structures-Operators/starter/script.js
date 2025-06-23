9a515ed34d0bacf00940c1f20749265287a9be3a
JorgeHumberto <jorge8hgn@hotmail.com>
2025-06-23 14:33:44 -0700

    23/06/25 - finising: section 09 - script.js

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -992,3 +992,28 @@ document.querySelector('button').addEventListener('click', function () {
 // Some_Variable;
 // calculate_AGE;
 // delayed_departure;
+
+// --
+// || ******************************************
+// LEC >> 132 - String Methods Practice
+// // || ******************************************
+// VV
+
+// // Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`
+    .trim()
+    .padStart(36);
+  // console.log(output);
+}
