0ff0d5002d0bdf8feb065e417f45441a55552d86
Jorge Humberto <jorge8hgn@hotmail.com>
2025-06-27 15:35:30 -0700

    27/06/25 - section 10_working: script.js

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -80,3 +80,52 @@ const newPassport = function (person) {
 // LEC >> 138 - Functions accepting callback functions
 // // || ******************************************
 // VV
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('🤚');
+};
+// document.body.addEventListener('click', high5);
+
+// ['Jonas', 'Martha'].forEach(high5);
+
+// --
+// || ******************************************
+// LEC >> 139 - Functions returning functions
+// // || ******************************************
+// VV
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Jorge');
+
+greet('Hello')('Jorge');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi')('Jorge');
