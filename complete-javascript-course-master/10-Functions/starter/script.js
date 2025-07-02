245ade5dc70346238d1c5a3751980bd9bf426b51
Jorge Humberto <jorge8hgn@hotmail.com>
2025-07-01 21:53:20 -0700

    01/07/25 - section 10_Lec144: script.js

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -149,8 +149,8 @@ const lufthansa = {
   },
 };
 
-lufthansa.book(239, 'Jorge Humberto');
-lufthansa.book(635, 'Hugo Gonzalez');
+// lufthansa.book(239, 'Jorge Humberto');
+// lufthansa.book(635, 'Hugo Gonzalez');
 
 const eurowings = {
   airline: 'Eurowings',
@@ -164,11 +164,11 @@ const book = lufthansa.book;
 // book(23, 'Sarah Williams');
 
 // Call method
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
 
-book.call(lufthansa, 239, 'Mary Cooper');
-console.log(lufthansa);
+// book.call(lufthansa, 239, 'Mary Cooper');
+// console.log(lufthansa);
 
 const swiss = {
   airline: 'Swiss Air Lines',
@@ -176,13 +176,165 @@ const swiss = {
   bookings: [],
 };
 
-book.call(swiss, 583, 'Mary Cooper');
-console.log(swiss);
+// book.call(swiss, 583, 'Mary Cooper');
+// console.log(swiss);
 
 // Apply method
 const flightData = [583, 'George Cooper'];
-book.apply(swiss, flightData);
-console.log(swiss);
+// book.apply(swiss, flightData);
+// console.log(swiss);
 
-book.call(swiss, ...flightData);
-console.log(swiss);
+// book.call(swiss, ...flightData);
+// console.log(swiss);
+
+// --
+// || ******************************************
+// LEC >> 141- The Bind method
+// // || ******************************************
+// VV
+
+const bookBW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+// // bookBW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+// // bookEW23('Jonas Schemedan');
+// // bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+// // console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 023;
+
+// // console.log(addVAT(100));
+// // console.log(addVAT(23));
+
+// Challenge
+// const addTax2 = function (rate, value) {
+//   return function (name) {
+//     console.log(`${rate} ${value} ${name}`);
+//   };
+// };
+// console.log(addTax2(0.2, 100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+// // console.log(addVAT2(100));
+// // console.log(addVAT2(23));
+
+// --
+// || ******************************************
+// LEC >> 142- Challenge #1
+// // || ******************************************
+// VV
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    //Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    //Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// // poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+// // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+// [5,2,3]
+// [1,5,3,9,6,1]
+
+// --
+// || ******************************************
+// LEC >> 143- Immediately Invoked Function Expressions (IIFE)
+// // || ******************************************
+// VV
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+// runOnce();
+
+// IIFE
+// (function () {
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+
+// console.log(isPrivate);
+
+// (() => console.log('This will never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+// console.log(notPrivate);
+
+// --
+// || ******************************************
+// LEC >> 144- Closures
+// // || ******************************************
+// VV
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
