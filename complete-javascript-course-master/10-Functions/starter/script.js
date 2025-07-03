111618abde42fa32f46d39f32a8c381c31814b7c
Jorge Humberto <jorge8hgn@hotmail.com>
2025-07-03 15:15:33 -0700

    03/07/25 - section 10_Lec146: script.js

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -335,6 +335,72 @@ const secureBooking = function () {
 
 const booker = secureBooking();
 
-booker();
-booker();
-booker();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// --
+// || ******************************************
+// LEC >> 145- More Closures Examples
+// // || ******************************************
+// VV
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+// g();
+// f();
+// console.dir(f);
+// Re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers.`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds.`);
+};
+
+// setTimeout(function(){
+//   console.log('TIMER');
+// }, 1000);
+
+const perGroup = 1000;
+// boardPassengers(180, 3);
+
+// --
+// || ******************************************
+// LEC >> 146- Challenging #2
+// // || ******************************************
+// VV
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'blue';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'red';
+  });
+})();
